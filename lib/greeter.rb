4bcfdbc6951cbc7bc0bf81a5f0bd718bc950838f
sarah-hoter <sarah.hoter@gmail.com>
2016-05-02 23:23:54 +0300

    Hello uses Greeter

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,8 +1,7 @@
-class Greeter
-  def initialize(who)
-    @who = who
-  end
-  def greet
-    "Hello, #{@who}"
-  end
-end
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
